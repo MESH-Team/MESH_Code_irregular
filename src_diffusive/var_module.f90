ce733a19247b5c43637237311ffd25b79b77dc54
nazmulazim <nazmul.azim@gmail.com>
2020-07-14 10:00:38 -0500

    Adding crank nicholson difusive wave scheme

---
+++ src_diffusive/var_module.f90
@@ -0,0 +1,11 @@
+module var_module
+
+    implicit none
+    save
+
+    integer :: ncomp, ots, option_dsbc
+    real(kind=4) :: cfl, qus, f, yds, rhs1, rhs2, c11, c12, c21, c22, us, thes
+    real(kind=4) :: vv
+    real(kind=4) :: dtini
+
+end module var_module
