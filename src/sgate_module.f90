d2d501893897266396808b98d6a1fc5babbc14bb
nazmulazim <nazmul.azim@gmail.com>
2022-03-25 08:45:58 -0500

    Making a final update
    Deleting unnecessary files

--- src/sgate_module.f90
+++
@@ -1,9 +0,0 @@
-module sgate_module
-
-    implicit none
-    save
-
-    integer :: option
-    real(kind=4) :: ag, bg, yn, eps, dmeu, yw, bw, ydsn
-
-end module sgate_module
--- src_diffusive/constants_module.f90
+++
@@ -1,9 +0,0 @@
-module constants_module
-
-    implicit none
-    save
-
-    real(kind=4), parameter :: grav = 9.81
-    real(kind=4), parameter :: TOLERANCE = 1e-8
-
-end module constants_module
